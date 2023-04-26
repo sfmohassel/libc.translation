9e24062d93b2f911ebf85f9d62c85290fea674d8
codengine <498798+codengine@users.noreply.github.com>
2023-04-27 00:30:21 +0200

    add flat-extracting json source

---
+++ libc.translation.tests/BaseLocalizationSourceTests.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Globalization;
+using Xunit;
+
+namespace libc.translation.tests
+{
+    public abstract class BaseLocalizationSourceTests
+    {
+        protected abstract ILocalizer GetLocalizer(PropertyCaseSensitivity caseSensitivity);
+        
+        [Fact]
+        public void CaseInsensitive()
+        {
+            var localizer = GetLocalizer(caseSensitivity: PropertyCaseSensitivity.CaseInsensitive);
+
+            // first....................................................
+            const string key1 = "InvalidInput";
+
+            // set thread's current culture to "ar"
+            CultureInfo.CurrentCulture = new CultureInfo(name: "ar");
+
+            Assert.Equal(
+                expected: "إدخال غير صالح",
+                actual: localizer.Get(key: key1),
+                comparer: StringComparer.Ordinal
+            );
+
+            Assert.Equal(
+                expected: "Ungültige Eingabe",
+                actual: localizer.Get(culture: "de", key: key1),
+                comparer: StringComparer.Ordinal
+            );
+
+            // set threads current culture to "en"
+            CultureInfo.CurrentCulture = new CultureInfo(name: "en");
+
+            Assert.Equal(
+                expected: "Invalid input",
+                actual: localizer.Get(key: key1),
+                comparer: StringComparer.Ordinal
+            );
+
+            // second.................................................
+            const string key2 = "unknownerror";
+
+            // set thread's current culture to "ar"
+            CultureInfo.CurrentCulture = new CultureInfo(name: "ar");
+
+            Assert.Equal(
+                expected: "خطأ غير معروف some word",
+                actual: localizer.GetFormat(key: key2, values: new object[]
+                {
+                    "some word"
+                }),
+                comparer: StringComparer.Ordinal
+            );
+
+            Assert.Equal(
+                expected: "Unbekannter Fehler test",
+                actual: localizer.GetFormat(culture: "de", key: key2, "test"),
+                comparer: StringComparer.Ordinal
+            );
+
+            // set threads current culture to "en"
+            CultureInfo.CurrentCulture = new CultureInfo(name: "en");
+
+            Assert.Equal(
+                expected: "Unknown error !!",
+                actual: localizer.GetFormat(key: key2, values: new object[]
+                {
+                    "!!"
+                }),
+                comparer: StringComparer.Ordinal
+            );
+
+            Assert.Equal(
+                expected: "Unbekannter Fehler !!",
+                actual: localizer.GetFormat(culture: "de", key: key2, "!!"),
+                comparer: StringComparer.Ordinal
+            );
+        }
+
+        [Fact]
+        public void CaseSensitive()
+        {
+            var localizer = GetLocalizer(caseSensitivity: PropertyCaseSensitivity.CaseSensitive);
+
+            // first...................................................
+            const string key1 = "InvalidInput";
+
+            // set thread's current culture to "ar"
+            CultureInfo.CurrentCulture = new CultureInfo(name: "ar");
+
+            Assert.Equal(
+                expected: "إدخال غير صالح",
+                actual: localizer.Get(key: key1),
+                comparer: StringComparer.Ordinal
+            );
+
+            Assert.Equal(
+                expected: "Ungültige Eingabe",
+                actual: localizer.Get(culture: "de", key: key1),
+                comparer: StringComparer.Ordinal
+            );
+
+            // set threads current culture to "en"
+            CultureInfo.CurrentCulture = new CultureInfo(name: "en");
+
+            Assert.Equal(
+                expected: "Invalid input",
+                actual: localizer.Get(key: key1),
+                comparer: StringComparer.Ordinal
+            );
+
+            // second..................................................
+            const string key2 = "unknownerror";
+
+            // set thread's current culture to "ar"
+            CultureInfo.CurrentCulture = new CultureInfo(name: "ar");
+
+            var x = localizer.GetFormat(key: key2, values: new object[]
+            {
+                "some word"
+            });
+
+            Assert.Equal(expected: key2, actual: x);
+        }
+
+        [Fact]
+        public void Nested_CaseSensitive()
+        {
+            var localizer = GetLocalizer(caseSensitivity: PropertyCaseSensitivity.CaseSensitive);
+
+            // set thread's current culture
+            CultureInfo.CurrentCulture = new CultureInfo(name: "de");
+
+            Assert.Equal(
+                expected: "Willkommen",
+                actual: localizer.Get(key: "home-page.title"),
+                comparer: StringComparer.Ordinal
+            );
+
+            Assert.Equal(
+                expected: "أهلا بك",
+                actual: localizer.Get(culture: "ar", key: "home-page.title"),
+                comparer: StringComparer.Ordinal
+            );
+
+            Assert.Equal(
+                expected: "Etwas Text",
+                actual: localizer.Get(key: "home-page.body.text"),
+                comparer: StringComparer.Ordinal
+            );
+
+            Assert.Equal(
+                expected: "بعض النصوص",
+                actual: localizer.Get(culture: "ar", key: "home-page.body.text"),
+                comparer: StringComparer.Ordinal
+            );
+        }
+
+        [Fact]
+        public void Nested_CaseInsensitive()
+        {
+            var localizer = GetLocalizer(caseSensitivity: PropertyCaseSensitivity.CaseInsensitive);
+
+            // set thread's current culture
+            CultureInfo.CurrentCulture = new CultureInfo(name: "de");
+
+            Assert.Equal(
+                expected: "Willkommen",
+                actual: localizer.Get(key: "home-page.title"),
+                comparer: StringComparer.Ordinal
+            );
+
+            Assert.Equal(
+                expected: "أهلا بك",
+                actual: localizer.Get(culture: "ar", key: "home-page.title"),
+                comparer: StringComparer.Ordinal
+            );
+
+            Assert.Equal(
+                expected: "Etwas Text",
+                actual: localizer.Get(key: "home-page.body.text"),
+                comparer: StringComparer.Ordinal
+            );
+
+            Assert.Equal(
+                expected: "بعض النصوص",
+                actual: localizer.Get(culture: "ar", key: "home-page.body.text"),
+                comparer: StringComparer.Ordinal
+            );
+        }
+
+        [Fact]
+        public void GetAll()
+        {
+            var localizer = GetLocalizer(caseSensitivity: PropertyCaseSensitivity.CaseSensitive);
+            CultureInfo.CurrentCulture = new CultureInfo(name: "de");
+            var allDe = localizer.GetAll();
+
+            Assert.Equal(
+                expected: 4,
+                actual: allDe.Keys.Count
+            );
+
+            Assert.Equal(
+                expected: "Ungültige Eingabe",
+                actual: allDe[key: "InvalidInput"],
+                comparer: StringComparer.Ordinal
+            );
+
+            Assert.Equal(
+                expected: "Unbekannter Fehler {0}",
+                actual: allDe[key: "UnknownError"],
+                comparer: StringComparer.Ordinal
+            );
+
+            Assert.Equal(
+                expected: "Willkommen",
+                actual: allDe[key: "home-page.title"],
+                comparer: StringComparer.Ordinal
+            );
+
+            Assert.Equal(
+                expected: "Etwas Text",
+                actual: allDe[key: "home-page.body.text"],
+                comparer: StringComparer.Ordinal
+            );
+        }
+    }
+}
---
+++ libc.translation.tests/FlatExtractingJsonLocalizationSourceTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace libc.translation.tests
+{
+    public class FlatExtractingJsonLocalizationSourceTests: BaseLocalizationSourceTests
+    {
+        protected override ILocalizer GetLocalizer(PropertyCaseSensitivity caseSensitivity)
+        {
+            return new Localizer(
+                source: new FlatExtractingJsonLocalizationSource(
+                    GetLocalPath(), caseSensitivity, "lang.{0}.json"
+                )
+            );
+        }
+
+        private static string GetLocalPath()
+        {
+            var assemblyPath = Assembly.GetExecutingAssembly().Location;
+            var uri = new UriBuilder(assemblyPath);
+            var assemblyDirectory = Uri.UnescapeDataString(uri.Path);
+
+            // Assuming the file is located in the same directory as the test assembly
+            return Path.GetDirectoryName(assemblyDirectory);
+        }
+    }
+}
